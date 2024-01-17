bfd0088fca5e2cbb26c39d9aa6e33e343c6e8ab0
TheKoray <62722522+TheKoray@users.noreply.github.com>
2024-01-17 07:49:44 +0300

    Update modelHelper.py

--- Scripts/modelHelper.py
+++ Scripts/modelHelper.py
@@ -80,7 +80,9 @@ class modelHelper():
         data_home = df.rename(columns = {'Home':'Team','HomeScore':'Score',"AwayScore":"ScoreCancel"})
         data_away = df.rename(columns = {'Away':'Team','AwayScore':'Score',"HomeScore":"ScoreCancel"})
 
-        result = pd.concat([data_home, data_away], ignore_index=True).groupby("Team").sum().loc[:,['Score','ScoreCancel']].sort_values(by = "Score", ascending=False)
+        result = pd.concat([data_home, data_away], ignore_index=True).groupby("Team")\
+                    .sum().loc[:,['Score','ScoreCancel']]\
+                    .sort_values(by = "Score", ascending=False)
         
         if home not in result.index:
             
@@ -111,7 +113,8 @@ class modelHelper():
         data_home = df.rename(columns = {'Home':'Team','HomePoint':'Point'})
         data_away = df.rename(columns = {'Away':'Team','AwayPoint':'Point'})
 
-        result = pd.concat([data_home, data_away], ignore_index=True).groupby("Team")["Point"].mean().to_frame("mbp")
+        result = pd.concat([data_home, data_away], ignore_index=True)\
+                    .groupby("Team")["Point"].mean().to_frame("mbp")
 
         if home not in result.index:
 
@@ -173,7 +176,9 @@ class modelHelper():
         data_home = mbp_data.rename(columns = {'Home':'Team','HomePoint':'Point'})
         data_away = mbp_data.rename(columns = {'Away':'Team','AwayPoint':'Point'})
 
-        result = pd.concat([data_home, data_away], ignore_index=True).groupby("Team")["Point"].mean().to_frame("mbp").apply(lambda x: round(x,2))
+        result = pd.concat([data_home, data_away], ignore_index=True)\
+                    .groupby("Team")["Point"].mean()\
+                    .to_frame("mbp").apply(lambda x: round(x,2))
 
         return result
 
@@ -182,7 +187,9 @@ class modelHelper():
         data_home = df.rename(columns = {'Home':'Team','HomeScore':'Score',"AwayScore":"ScoreCancel"})
         data_away = df.rename(columns = {'Away':'Team','AwayScore':'Score',"HomeScore":"ScoreCancel"})
 
-        result = pd.concat([data_home, data_away], ignore_index=True).groupby("Team").sum().loc[:,['Score','ScoreCancel']].sort_values(by = "Score", ascending=False)
+        result = pd.concat([data_home, data_away], ignore_index=True)\
+                    .groupby("Team").sum().loc[:,['Score','ScoreCancel']]\
+                    .sort_values(by = "Score", ascending=False)
         return result
     
     def predResult(self, home, away, model,team_data, play_df):
